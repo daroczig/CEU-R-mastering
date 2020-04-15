c5289cae9feda48fe8045daaf7ac39cacaed9a15
miklosbona <miklos.bona@gmail.com>
2020-04-15 12:40:35 +0000

    comments added

--- class1.R
+++ class1.R
@@ -1,13 +1,18 @@
 library(binancer)
 library(data.table)
-a <- binance_klines('BTCUSDT', interval = '1m')
+library(logger)
 
-a$close
+BITCOINS <- 0.42
+log_info('Number of Bitcoins: {BITCOINS}')
 
+# get prices from Binance
 prices <- binance_ticker_all_prices()
 prices
+
+# search for BTC, USD
 btc_usd <- prices[from == 'BTC' & to == 'USDT', price]
-ex1 <- btc_usd * 0.42
+ex1 <- btc_usd * BITCOINS
+log_info('BTCUSDT: {btc_usd}')
 
 # how to get currency rates?
 readLines('https://api.exchangeratesapi.io/latest?base=USD')
@@ -15,7 +20,10 @@ readLines('https://api.exchangeratesapi.io/latest?base=USD')
 # parse the json object
 library(jsonlite)
 
+# FX rate USDHUF
 usd_huf <- fromJSON('https://api.exchangeratesapi.io/latest?base=USD')$rates$HUF
+log_info('USDHUF: {usd_huf}')
 
-
+# solution for the 2nd exercise
 ex_2 <- ex1*usd_huf
+ex_2
