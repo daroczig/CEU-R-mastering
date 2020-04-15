aa16e27f633efc0d5ad45a4210c5624ee89f48c6
miklosbona <miklos.bona@gmail.com>
2020-04-15 13:04:48 +0000

    log_eval added

--- class1.R
+++ class1.R
@@ -6,16 +6,16 @@ library(checkmate)
 BITCOINS <- 0.42
 log_info('Number of Bitcoins: {BITCOINS}')
 
-# get prices from Binance - if there is an error: use tryCatch
+## get prices from Binance - if there is an error: use tryCatch
 prices <- binance_ticker_all_prices()
 prices
 
-# search for BTC, USD
+## search for BTC, USD
 btc_usd <- prices[from == 'BTC' & to == 'USDT', price]
 ex1 <- btc_usd * BITCOINS
 log_info('BTCUSDT: {btc_usd}')
 
-# tryCatch
+## tryCatch
 get_bitcoin_price <- function() {
   tryCatch(
   binance_coins_prices()[symbol == 'BTC', usd],
@@ -26,10 +26,10 @@ btcusd <- get_bitcoin_price()
 
 log_info('BTCUSDT: {btcusd}')
 
-# check the values from the function
+## check the values from the function
 assert_number(btcusd, lower = 10000)
 
-# how to get currency rates?
+## how to get currency rates?
 readLines('https://api.exchangeratesapi.io/latest?base=USD')
 
 # parse the json object
@@ -40,6 +40,13 @@ usd_huf <- fromJSON('https://api.exchangeratesapi.io/latest?base=USD')$rates$HUF
 log_info('USDHUF: {usd_huf}')
 assert_number(usd_huf, lower = 250 , upper = 500)
 
-# solution for the 2nd exercise
-ex_2 <- ex1*usd_huf
+## solution for the 2nd exercise
+ex_2 <- ex1 * usd_huf
 ex_2
+
+log_info(ex_2)
+
+## setting the log levels:
+log_threshold(TRACE) # better than log_info - it is less chatty
+log_eval(ex_2)
+
